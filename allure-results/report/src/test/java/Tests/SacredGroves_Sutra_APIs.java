86041b5772377bed74bf1fee72e3ead8e2676098
Dharma <seo@frugaltesting.com>
2025-05-26 18:36:43 +0530

    Automated report update for 26052025

---
+++ allure-results/report/src/test/java/Tests/SacredGroves_Sutra_APIs.java
@@ -0,0 +1,101 @@
+package Tests;
+
+import Base.BaseTest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+import static io.restassured.RestAssured.given;
+
+public class SacredGroves_Sutra_APIs extends BaseTest {
+    String BaseURL = "https://devoutshade.sacredgroves.earth/api";
+    String patchToken = "";
+    String workbook = "C:\\Users\\hp\\IdeaProjects\\untitled6\\src\\main\\resources\\Patch_Users.xlsx";
+
+    @BeforeClass
+    public void User() {
+        patchToken = super.PatchUser().jsonPath().getString("data");
+    }
+
+    @Test
+    public void clusterDetails() {
+        String  url = "https://devoutshade.sacredgroves.earth/api/sutra/cluster/details";
+
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + patchToken).
+                get(url);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected status code is " + 200 + " but the actual status code is " + response.statusCode());
+        String token = response.jsonPath().getString("data");
+        System.out.println(token);
+    }
+    @Test
+    public void clusterTrend() {
+        String  url = "https://devoutshade.sacredgroves.earth/api/sutra/forest/cluster/trend/";
+
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + patchToken).
+                get(url);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected status code is " + 200 + " but the actual status code is " + response.statusCode());
+        String token = response.jsonPath().getString("data");
+        System.out.println(token);
+    }
+    @Test
+    public void powerBi() {
+        String url = "https://devoutshade.sacredgroves.earth/api/sutra/powerbi";
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                get(url);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected Status code is " + 200 + " but the actual status Code is" + response.statusCode());
+        String token = response.jsonPath().getString("data");
+        System.out.println(token);
+    }
+    @Test
+    public void mapCluster() {
+        String url = "https://devoutshade.sacredgroves.earth/api/sutra/map/cluster?forestId=UNI_GIGR_00001&iframe_height=250&iframe_width=450&zoom_start=17";
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                get(url);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+//        if (!(response.statusCode() == 200))
+//            Assert.fail("Expected Status code is " + 200 + " but the actual status Code is" + response.statusCode());
+        String token = response.jsonPath().getString("data");
+        System.out.println(token);
+    }
+    @Test
+    public void sutraMapForest() {
+        String url = "https://devoutshade.sacredgroves.earth/api/sutra/map/forest?forestId=UNI_GIGR_00001&iframe_height=250&iframe_width=450&zoom_start=17";
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                get(url);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected Status code is " + 200 + " but the actual status Code is" + response.statusCode());
+
+    }
+}
---
+++ allure-results/report/src/test/java/Tests/Sacred_Admin.java
@@ -0,0 +1,193 @@
+package Tests;
+
+import Base.BaseTest;
+import SheetAdmin.admin_DEL;
+import SheetAdmin.admin_GET;
+import SheetAdmin.admin_POST;
+import SheetAdmin.admin_post_Invite;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Sacred_Admin extends BaseTest {
+    String TOKEN;
+    String BaseURL = "https://devoutshade.sacredgroves.earth/api/admins";
+
+
+    @BeforeClass
+    public void Token() {
+        TOKEN = super.getToken().jsonPath().getString("data");
+    }
+
+    @Test
+    public void AdminLogin() {
+
+        System.out.println(TOKEN);
+
+    }
+
+
+    @Test
+    public void GetAllUsers() {
+        String path = "/users";
+
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + TOKEN)
+                .get(BaseURL + path);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected status code is " + 200 + " but the actual status code is " + response.statusCode());
+
+    }
+
+
+    @Test(dataProvider = "adminInvite")
+    public void POST_Send_Invite() {
+        String path = "/invite";
+
+        String requestBody = "{\"email\":[\"amitthakurashwani@gmail.com\" ,\"amit@outshade.com\"]}";
+
+        System.out.println(requestBody);
+
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + TOKEN)
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post(BaseURL + path);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        Assert.assertEquals(response.getStatusCode(),200);
+
+    }
+    @Test
+    public void Delete_User()
+    {
+        String path = "/user/{id}";
+        String user_id = "586717a0-2370-4590-9882-d66335cc27fe";
+        Response response = given()
+                .header("Authorization", "Bearer " + TOKEN)
+                .pathParams("id",user_id)
+                .delete(BaseURL + path);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+
+    @Test
+    public void GetAllUsersWithAmount() {
+        String path = "/users/3/0";
+
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + TOKEN)
+                .get(BaseURL + path);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected status code is " + 200 + " but the actual status code is " + response.statusCode());
+
+    }
+    @Test
+    public void GetParticularUserDetails() {
+        String path = "/user/{id}";
+        String user_id = "ae46383c-8e83-4bac-8968-2078f648db47";
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + TOKEN)
+                .pathParams("id",user_id)
+                .get(BaseURL + path);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected status code is " + 200 + " but the actual status code is " + response.statusCode());
+
+    }
+
+    @Test
+    public void activities() {
+        String path = "/activities";
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + TOKEN)
+                .get(BaseURL + path);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected status code is " + 200 + " but the actual status code is " + response.statusCode());
+
+    }
+
+    @Test
+    public void Squads() {
+        String path = "/squads";
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + TOKEN).
+                get(BaseURL + path);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected status code is " + 200 + " but the actual status code is " + response.statusCode());
+
+    }
+    @Test
+    public void grovepoints() {
+        String path = "/grovepoints";
+        Response response = given().
+                contentType(ContentType.JSON).
+                accept("application/json").
+                header("Authorization", "Bearer " + TOKEN).
+                get(BaseURL + path);
+        System.out.println(response.statusCode());
+        response.then().log().all();
+        response.prettyPrint();
+        if (!(response.statusCode() == 200))
+            Assert.fail("Expected status code is " + 200 + " but the actual status code is " + response.statusCode());
+
+    }
+    @DataProvider(name = "getCall")
+    public Object[][] getCall() {
+        Object[][] getCall = admin_GET.ExcelData();
+        return getCall;
+    }
+
+
+    @DataProvider(name = "adminPostCall")
+    public Object[][] adminPostCall() {
+        Object[][] adminPostCall = admin_POST.ExcelData();
+        return adminPostCall;
+    }
+
+    @DataProvider(name = "adminInvite")
+    public Object[][] adminInvite() {
+        Object[][] POST_Send_Invite = admin_post_Invite.ExcelData();
+        return POST_Send_Invite;
+    }
+    @DataProvider(name = "adminDelete")
+    public Object[][] adminDelete() {
+        Object[][] Delete_User = admin_DEL.ExcelData();
+        return Delete_User;
+    }
+
+}
